889a39ea6d0824d0afc7b15c6f028d7d147fda7d
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-08-21 17:53:47 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountpassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+//    accountId = 2  // not allowed
+
+
+/*
+prefer not to use var
+because of iisue in block scope and functional scope
+
+*/
+   accountEmail = "hc@hc.com"
+
+   
+   accountpassword = "212112"
+   accountCity = "bangluru"
+   console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
+
+
