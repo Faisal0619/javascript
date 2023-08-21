39f82cc6e4263a86f07159cea5eaaf1713292d3a
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-08-21 18:13:35 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all Js code as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+
+console.log(3
+    +
+    3); // code readability should be high
+
+console.log("hitesh");
+// Documentation  mdn  ,https://tc39.es/ecma262/2023/
+
+let name = "Faisal"
+let age = 18
+let isloggedIn = false
+
+
+// number => 2 to power 53
+// bigint mostly used in trading,stock market
+//string =>""
+// boolean =>true/false
+//null =>standalone  value
+//undefined =>
+//symbol =>unique 
+
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
