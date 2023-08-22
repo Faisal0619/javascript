390695b39c876a0cd3d4efa6ddd731486ce86f4b
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-08-22 17:09:16 +0000

    datatypes Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,60 @@
+//  primitive  =>call by value
+
+// 7  types : String , Number,Boolean, null(empty), undefined, Symbol,BigInt
+
+   const score = 100 // return type Number
+   const scoreValue = 100.3 //return type Number
+
+   const isLoggedIn = false
+   const outSideTemp = null // Object
+   let userEmail;
+
+
+   const id = Symbol('123') // Symbol
+
+   const anotherId =Symbol('123')     // Symbol
+
+
+   console.log(id === anotherId);   //False
+
+   const bigNumber = 32458787875n     // BigInt
+
+
+
+
+
+
+//Reference type (Non Primitive)
+// Array , Objects, Functions
+
+            const heros= ["shaktiman","naagraj","doga"];// array
+
+      let myObj=      {
+                name: "faisal",   // object
+                age:18,
+            }
+               
+           const myFunction= function()
+            {
+console.log("Hello World");
+            }
+      
+
+            console.log(typeof anotherId);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
