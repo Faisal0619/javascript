b0f7856e1a876adceb1a4a757c5d2a286d73f55c
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-08-22 15:46:11 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,39 @@
+let score = "faisal"
+
+// "33abc" is not a number it NaN
+ //const {score} =    req.body
+
+
+ //console.log(typeof score);
+ //console.log(typeof (score));
+
+
+ let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+ //console.log((valueInNumber));
+
+/*
+"33" => 33
+"33abe"  => NaN
+true => 1; false => 0
+
+*/
+
+let isLoggedIn = "faisal"
+  let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ // console.log( (booleanIsLoggedIn));
+
+
+  // 1 => true; 0 => false
+  // "" => false
+  // "faisal" => true
+
+  let someNumber = 33
+  let stringNumber = String(someNumber)
+
+  console.log(typeof stringNumber);
+
+
+
+  
