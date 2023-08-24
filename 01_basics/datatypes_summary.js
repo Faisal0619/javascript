6568d6823f715b60cae93a31db1cd13aab62de8e
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-08-24 17:19:01 +0000

    continue

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -15,7 +15,7 @@
    const anotherId =Symbol('123')     // Symbol
 
 
-   console.log(id === anotherId);   //False
+  // console.log(id === anotherId);   //False
 
    const bigNumber = 32458787875n     // BigInt
 
@@ -36,14 +36,38 @@
                
            const myFunction= function()
             {
-console.log("Hello World");
+//console.log("Hello World");
             }
       
 
-            console.log(typeof anotherId);
+          //  console.log(typeof anotherId);
 
 
-            
+            //************************************************************************************//
+
+// Stack(primitive), Heap(Non-primitive)
+
+
+let myYoutubeName = "faisalrashid.com";
+
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode";
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+   email: "useratgoogle.com",
+   upi:"user@ybl"
+}
+let userTwo = userOne;
+userTwo.email = "hitest@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
 
 
 
