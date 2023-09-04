9905bc8682f491e7761222d3e07caff69888d41c
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-09-04 17:27:14 +0000

    objects destructuring json(api)

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -73,7 +73,7 @@ const tinderUser  =  {}
     }
  }
 
- console.log(regularUser.fullname.userfullname.lastname);
+ //console.log(regularUser.fullname.userfullname.lastname);
 
 
 
@@ -84,7 +84,7 @@ const tinderUser  =  {}
  //const obj3 = {obj1 ,obj2}
 //const obj3 =Object.assign({},obj1 , obj2)
 const obj3 = {...obj1,...obj2}
- console.log(obj3);
+// console.log(obj3);
 
 
  const users = [
@@ -102,10 +102,58 @@ const obj3 = {...obj1,...obj2}
 
 
 users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+//de-structure
+const course =
+{
+courseName: "js in hindi",
+price : "999",
+courseInstructor : "Hitesh"
+}
+
+//course.courseInstructor
+
+const {courseInstructor: instructor} = course 
+
+//console.log(courseInstructor);
+
+console.log(instructor);
+
+
+const navbar = ({company}) => {                   // instead of (props.company)
+
+}                                                  // de-structuring
+navbar(company = "hitesh")
+
+
+
+// {
+//     "name":"hitesh",                              // api (json)= javascipt object notation
+//     "coursename" : "js in hindi",                 // fetch method
+//     "price" : "free"
+
+// }
+[
+{},
+{},
+{}
+]
+
+
+
+
+
+
+
+
+
+
+
 
 
