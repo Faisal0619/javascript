8cbdd57775c5e07706ec76b288758d8ad0f036fc
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-09-08 17:15:53 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,65 @@
+const myObject = {
+    js: 'javascript',
+    cpp:'c++',
+    rb: 'ruby',
+    swift: ' swift by apple'
+
+}
+for (const key in myObject) {
+   // console.log(myObject[key]);
+   // console.log(`${key}  shortcut is for ${myObject[key]}`);
+}
+const programming = ["js","rb","cpp","java"]
+for (const key in programming) {
+      //  console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('USA',"UNITED STATES OF AMERICA")
+// map.set('IN',"INDIA")
+// map.set('FR',"FRANCE")
+// map.set('IN',"INDIA")
+
+// for (const key in map) {
+//  console.log(key);  // not iterable
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
