55f0b67b074fa44e316fe86b8c1f323a9beca543
Faisal Rashid <106532056+Faisal0619@users.noreply.github.com>
2023-09-08 17:16:31 +0000

    for each loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,70 @@
+const coding = ["js","rb","cpp","java","python"]
+
+// array.forEach(element => {
+    
+// });                                   // callback function has no name
+// coding.forEach( function (item) {
+
+// console.log(item);
+
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// } 
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item,index,arr)=>{
+//     console.log(item,index,arr);
+
+// })
+
+const myCoding = [
+    {
+        languageName:"javascipt",
+        languageFilename:"js"
+        
+    },
+    {
+        languageName:"java",
+        languageFilename:"java"
+
+    },
+    {
+        languageName:"python",
+        languageFilename:"py"
+
+    },
+]
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
